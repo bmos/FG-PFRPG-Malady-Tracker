9c6ba35012f9273d820ee63635f8132946704196
Wil Thieme <wil.thieme@protonmail.com>
2020-07-27 19:22:52 -0400

    handle frequency and duration
    add button to add 50% to duration (only shown if malady is poison)

--- campaign/scripts/diseasedesc_stats.lua
+++ campaign/scripts/diseasedesc_stats.lua
@@ -7,6 +7,38 @@ function onInit()
 end
 
 --	This function takes the Save DC related information and combines them into a single string that can be displayed once the window is locked.
+function generateDurationString()
+	local sDur = ''
+	
+	if duration_interval.getValue() and duration_interval.getValue() ~= 0 and duration_unit.getValue() and duration_unit.getValue() ~= '' then
+		sDur = duration_interval.getValue()
+	end
+	if duration_unit.getValue() and duration_unit.getValue() ~= '' then
+		sDur = sDur .. ' ' .. duration_unit.getValue()
+	else
+		sDur = 'none'
+	end
+	
+	duration_string.setValue(sDur)
+end
+
+--	This function takes the Save DC related information and combines them into a single string that can be displayed once the window is locked.
+function generateFrequencyString()
+	local sFreq = ''
+	
+	if freq_interval.getValue() and freq_interval.getValue() ~= 0 and freq_unit.getValue() and freq_unit.getValue() ~= '' then
+		sFreq = freq_interval.getValue()
+	end
+	if freq_unit.getValue() and freq_unit.getValue() ~= '' then
+		sFreq = sFreq .. freq_unit.getValue()
+	else
+		sFreq = 'none'
+	end
+	
+	freq_string.setValue(sFreq)
+end
+
+--	This function takes the Save DC related information and combines them into a single string that can be displayed once the window is locked.
 function generateSaveString()
 	local sSave = ''
 	
@@ -23,14 +55,14 @@ function generateSaveString()
 end
 
 ---	This function hides extra fields once the malady type is set.
-function switchType()
-	local sType = string.lower(type.getValue())
+function switchType(sType)
 	if sType == 'poison' then
 		disease_effect.setVisible(false)
 		poison_effect_primary_label.setVisible(true)
 		poison_effect_primary.setVisible(true)
 		poison_effect_secondary_label.setVisible(true)
 		poison_effect_secondary.setVisible(true)
+		
 	elseif sType == 'disease' then
 		disease_effect.setVisible(true)
 		poison_effect_primary_label.setVisible(false)
@@ -47,7 +79,7 @@ function switchType()
 end
 
 --	This function sets the visibility and editability of various fields on the malady sheet when it is unlocked.
-local function ifLocked()
+local function ifLocked(sType)
 	local sSubtype = ''
 	if subtype.getValue() and subtype.getValue() ~= '' then
 		sSubtype = ' (' .. subtype.getValue() .. ')'
@@ -55,13 +87,28 @@ local function ifLocked()
 	type_biglabel.setValue('[' .. type.getValue() .. sSubtype .. ']')
 
 	generateSaveString()
+	generateFrequencyString()
+	generateDurationString()
 
 	save_string.setVisible(true)
 	savetype.setVisible(false)
 	savedc_label.setVisible(false)
 	savedc.setVisible(false)
 	saveroll.setVisible(true)
-	raisesave.setVisible(true)
+
+	if sType ~= 'disease' then
+		if save_string.getValue() ~= 'none' then raisesave.setVisible(true) end
+		duration_label.setVisible(false)
+		duration_string.setVisible(true)
+		duration_interval.setVisible(false)
+		duration_unit.setVisible(false)
+		if duration_string.getValue() ~= 'none' then increaseduration.setVisible(true) end
+	end
+
+	freq_label.setVisible(false)
+	freq_string.setVisible(true)
+	freq_unit.setVisible(false)
+	freq_interval.setVisible(false)
 
 	type_biglabel.setVisible(true)
 	type.setVisible(false)
@@ -71,13 +118,26 @@ local function ifLocked()
 end
 
 --	This function sets the visibility and editability of various fields on the malady sheet when it is locked.
-local function ifUnlocked()
+local function ifUnlocked(sType)
 	save_string.setVisible(false)
 	savetype.setVisible(true)
 	savedc_label.setVisible(true)
 	savedc.setVisible(true)
 	saveroll.setVisible(false)
 	raisesave.setVisible(false)
+
+	if sType ~= 'disease' then
+		duration_label.setVisible(true)
+		duration_string.setVisible(false)
+		duration_interval.setVisible(true)
+		duration_unit.setVisible(true)
+		increaseduration.setVisible(false)
+	end
+
+	freq_label.setVisible(true)
+	freq_string.setVisible(false)
+	freq_unit.setVisible(true)
+	freq_interval.setVisible(true)
 
 	type_biglabel.setVisible(false)
 	type.setVisible(true)
@@ -89,19 +149,19 @@ end
 
 function update()
 	local bReadOnly = WindowManager.getReadOnlyState(getDatabaseNode())
+	local sType = string.lower(type.getValue())
 	if bReadOnly then
-		ifLocked()
+		ifLocked(sType)
 	else
-		ifUnlocked()
+		ifUnlocked(sType)
 	end
 
 	onset.update(bReadOnly)
-	frequency.update(bReadOnly)
 	cure.update(bReadOnly)
 	disease_effect.update(bReadOnly)
 	poison_effect_primary.update(bReadOnly)
 	poison_effect_secondary.update(bReadOnly)
 	description.update(bReadOnly)
 
-	switchType()
+	switchType(sType)
 end
