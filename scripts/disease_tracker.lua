32c662ce9fcf43aba8068d814e684a6b34f29235
Wil Thieme <wil.thieme@protonmail.com>
2020-08-03 18:14:10 -0400

    roll saves due after clock is advanced
    requires the ClockAdjuster extension

--- scripts/disease_tracker.lua
+++ scripts/disease_tracker.lua
@@ -2,6 +2,40 @@
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
+function onInit()
+	if TimeManager then DB.addHandler("calendar.current", "onChildUpdate", onTimeChanged) end
+end
+
+function onTimeChanged(node)
+	local nDateinMinutes = TimeManager.getCurrentDateinMinutes()
+	for _,vNode in pairs(DB.getChildren(node.getChild('...'), "charsheet")) do 	-- iterates through each player character
+		for _,vvNode in pairs(DB.getChildren(vNode, "diseases")) do 			-- iterates through each disease of the player character
+			local nDateOfContr = DB.getValue(vvNode, 'starttime')
+			if nDateOfContr and nDateinMinutes then								-- if the disease has a date of contraction listed and the current date is known
+				local nTimeElapsed = nDateinMinutes - nDateOfContr
+				local nFreq = DB.getValue(vvNode, 'remindercycle', 1)
+				local nPrevRollCount = DB.getValue(vvNode, 'savecount', 0)
+				local nNewRollCount = math.floor(nTimeElapsed / nFreq)
+				
+				if DB.getValue(vvNode, 'savetype') and nNewRollCount > nPrevRollCount then	-- if savetype is known and more saves are due
+					local nRollCount = 0
+					repeat														-- rolls saving throws until the correct total number have been rolled
+						local rActor = ActorManager.getActor('pc', vNode)
+						local sSave = DB.getValue(vvNode, 'savetype')
+						local nDC = DB.getValue(vvNode, 'savedc')
+						local sType = DB.getValue(vvNode, 'type')
+						rollSave(rActor, sSave, nDC, sType)
+
+						nRollCount = nRollCount + 1
+					until nRollCount == (nNewRollCount - nPrevRollCount)
+
+					DB.setValue(vvNode, 'savecount', 'number', nPrevRollCount + nRollCount)	-- saves the new total for use next time
+				end
+			end
+		end
+	end
+end
+
 --- Allow dragging and dropping madnesses between players
 --	@return nodeChar This is the databasenode of the player character within charsheet.
 --	@return sClass 
