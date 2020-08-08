88a8a2dd18b65ed643d0c423b043151857eed6af
Wil Thieme <wil.thieme@protonmail.com>
2020-08-08 18:51:09 -0400

    fix strange behavior on 1st on the month

--- scripts/disease_tracker.lua
+++ scripts/disease_tracker.lua
@@ -3,7 +3,7 @@
 --
 
 function onInit()
-	if TimeManager then DB.addHandler('calendar.current', 'onChildUpdate', onTimeChanged) end
+	if TimeManager then DB.addHandler('calendar.dateinminutes', 'onUpdate', onTimeChanged) end
 end
 
 ---	This function is called by the handler which watches for changes in current time.
