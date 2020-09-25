edc661800de9feaed42ac2bcf2883c81c188b3ff
Wil Thieme <wil.thieme@protonmail.com>
2020-09-25 07:55:30 -0400

    include rounds in auto-set starttime

--- campaign/scripts/adddisease.lua
+++ campaign/scripts/adddisease.lua
@@ -32,8 +32,9 @@ local function addDisease(nodeChar, sClass, sRecord, nodeTargetList)
 				ActionDiseaseTimeRoll.performRoll(draginfo, nodeEntry, rActor, DB.getValue(nodeSource, 'duration_dice'), DB.getValue(nodeSource, 'duration_interval'), 'Duration')
 			end
 		end
-		if TimeManager and DB.getValue(nodeEntry, 'freq_interval') and tonumber(DB.getValue(nodeEntry, 'freq_unit', .1)) >= 1 then
-			DB.setValue(nodeEntry, 'starttime', 'number', TimeManager.getCurrentDateinMinutes())
+		if TimeManager and DB.getValue(nodeEntry, 'freq_interval') and tonumber(DB.getValue(nodeEntry, 'freq_unit', 0.1)) >= 1 then
+			local nRound = DB.getValue(nodeEntry.getChild('.....'), 'combattracker.round', 1)
+			DB.setValue(nodeEntry, 'starttime', 'number', TimeManager.getCurrentDateinMinutes() + ( 0.1 * nRound ))
 			DB.setValue(nodeEntry, 'savecount', 'number', 0)
 		end
 	else
