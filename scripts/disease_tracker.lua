8a83b7577d207cf5a1c3c3d139d2867659b87d7e
Wil Thieme <wil.thieme@protonmail.com>
2022-04-09 12:57:52 -0400

    restruct function scopes

--- scripts/disease_tracker.lua
+++ scripts/disease_tracker.lua
@@ -1,20 +1,6 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
-function onInit()
-	if TimeManager_Disabled and LongTermEffects then DB.addHandler('calendar.dateinminutes', 'onUpdate', parseDiseases) end
-
-	LibraryData.setRecordTypeInfo(
-					'disease', {
-						bExport = true,
-						aDataMap = { 'disease', 'reference.diseases' },
-						sRecordDisplayClass = 'referencedisease',
-						aGMListButtons = { 'button_feat_type' },
-						aPlayerListButtons = { 'button_feat_type' },
-						aCustomFilters = { ['Type'] = { sField = 'type' } },
-					}
-	)
-end
 
 ---	This function rounds to the specified number of decimals
 --	luacheck: globals round
@@ -29,48 +15,8 @@ function round(number, decimals)
 	return number / n
 end
 
---	luacheck: globals calculateFrequency
-function calculateFrequency(nodeDisease)
-	local nFreqUnit = tonumber(DB.getValue(nodeDisease, 'freq_unit'))
-	local nFreqVal = DB.getValue(nodeDisease, 'freq_interval')
-	local nFreq
-	-- Debug.console(sDiseaseName, 'nFreqUnit: ' .. nFreqUnit, 'nFreqVal: ' .. nFreqVal)
-	if nFreqUnit and nFreqVal then nFreq = nFreqUnit * nFreqVal end
-	-- Debug.console(sDiseaseName, 'Frequency: ' .. nFreq or 'nil')
-
-	return nFreq
-end
-
---	luacheck: globals calculateDuration
-function calculateDuration(nodeDisease, nOnset)
-	local nDurUnit = tonumber(DB.getValue(nodeDisease, 'duration_unit'))
-	local nDurVal = DB.getValue(nodeDisease, 'duration_interval')
-	local nDuration, nDurationWithOnset
-	-- Debug.console(sDiseaseName, 'nDurUnit: ' .. nDurUnit, 'nDurVal: ' .. nDurVal)
-	if nDurUnit and nDurVal then
-		nDuration = nDurUnit * nDurVal
-		nDurationWithOnset = nDurUnit * nDurVal + nOnset
-	end
-	-- Debug.console(sDiseaseName, 'nDurationWithOnset: ' .. nDurationWithOnset, 'nDuration: ' .. nDuration)
-
-	return nDurationWithOnset, nDuration, nDurUnit
-end
-
---	luacheck: globals calculateOnset
-function calculateOnset(nodeDisease)
-	local nOnsUnit = tonumber(DB.getValue(nodeDisease, 'onset_unit'))
-	local nOnsVal = DB.getValue(nodeDisease, 'onset_interval')
-	local nOnset
-	-- Debug.console(sDiseaseName, 'nOnsUnit: ' .. nOnsUnit, 'nOnsVal: ' .. nOnsVal)
-	if nOnsUnit and nOnsVal then nOnset = nOnsUnit * nOnsVal end
-	-- Debug.console(sDiseaseName, 'nOnset: ' .. nOnset)
-
-	return nOnset
-end
-
 -- This function iterates through each disease and poison of each character
---	luacheck: globals parseDiseases
-function parseDiseases()
+local function parseDiseases()
 	local nDateWithRounds = TimeManager.getCurrentDateinMinutes()
 	for _, nodeCT in pairs(DB.getChildren(CombatManager.CT_LIST)) do
 		local rActor = ActorManager.resolveActor(nodeCT)
@@ -80,6 +26,17 @@ function parseDiseases()
 			if nDateOfContr and DB.getValue(nodeDisease, 'savetype') and not string.find(DB.getValue(nodeDisease, 'name', ''), '%[') then
 				local nTimeSinceContraction = nDateWithRounds - nDateOfContr
 
+				local function calculateOnset(nodeDisease)
+					local nOnsUnit = tonumber(DB.getValue(nodeDisease, 'onset_unit'))
+					local nOnsVal = DB.getValue(nodeDisease, 'onset_interval')
+					local nOnset
+					-- Debug.console(sDiseaseName, 'nOnsUnit: ' .. nOnsUnit, 'nOnsVal: ' .. nOnsVal)
+					if nOnsUnit and nOnsVal then nOnset = nOnsUnit * nOnsVal end
+					-- Debug.console(sDiseaseName, 'nOnset: ' .. nOnset)
+
+					return nOnset
+				end
+
 				local nOnset = calculateOnset(nodeDisease) or 0
 				local nDiseaseElapsed = round(nTimeSinceContraction - nOnset, 1)
 
@@ -87,6 +44,18 @@ function parseDiseases()
 				Debug.console(sDiseaseName, 'nDiseaseElapsed: ' .. nDiseaseElapsed)
 				-- if the disease has a starting time, the current time is known, and any onset has elapsed
 				if nDiseaseElapsed > 0 then
+
+					local function calculateFrequency(nodeDisease)
+						local nFreqUnit = tonumber(DB.getValue(nodeDisease, 'freq_unit'))
+						local nFreqVal = DB.getValue(nodeDisease, 'freq_interval')
+						local nFreq
+						-- Debug.console(sDiseaseName, 'nFreqUnit: ' .. nFreqUnit, 'nFreqVal: ' .. nFreqVal)
+						if nFreqUnit and nFreqVal then nFreq = nFreqUnit * nFreqVal end
+						-- Debug.console(sDiseaseName, 'Frequency: ' .. nFreq or 'nil')
+
+						return nFreq
+					end
+
 					local nFreq = calculateFrequency(nodeDisease)
 					if nFreq then
 						local nPrevRollCount = DB.getValue(nodeDisease, 'savecount', 0) -- how many saves have been successful
@@ -94,6 +63,21 @@ function parseDiseases()
 						-- Debug.console(sDiseaseName, 'nTotalRolls: ' .. nTotalRolls, 'nPrevRollCount: ' .. nPrevRollCount)
 						local nTargetRollCount = nTotalRolls - nPrevRollCount
 						if nTargetRollCount > 0 then
+
+							local function calculateDuration(nodeDisease, nOnset)
+								local nDurUnit = tonumber(DB.getValue(nodeDisease, 'duration_unit'))
+								local nDurVal = DB.getValue(nodeDisease, 'duration_interval')
+								local nDuration, nDurationWithOnset
+								-- Debug.console(sDiseaseName, 'nDurUnit: ' .. nDurUnit, 'nDurVal: ' .. nDurVal)
+								if nDurUnit and nDurVal then
+									nDuration = nDurUnit * nDurVal
+									nDurationWithOnset = nDurUnit * nDurVal + nOnset
+								end
+								-- Debug.console(sDiseaseName, 'nDurationWithOnset: ' .. nDurationWithOnset, 'nDuration: ' .. nDuration)
+
+								return nDurationWithOnset, nDuration, nDurUnit
+							end
+
 							local nDurationWithOnset, nDuration, _ = calculateDuration(nodeDisease, nOnset)
 							if nDurationWithOnset then nTargetRollCount = math.min(nTargetRollCount, nDuration / nFreq); end
 							-- Debug.console(sDiseaseName, 'nDurationWithOnset: ' .. nDurationWithOnset, 'nDiseaseElapsed: ' .. nDiseaseElapsed)
@@ -142,4 +126,19 @@ function parseDiseases()
 			end
 		end
 	end
+end
+
+function onInit()
+	if TimeManager_Disabled and LongTermEffects then DB.addHandler('calendar.dateinminutes', 'onUpdate', parseDiseases) end
+
+	LibraryData.setRecordTypeInfo(
+					'disease', {
+						bExport = true,
+						aDataMap = { 'disease', 'reference.diseases' },
+						sRecordDisplayClass = 'referencedisease',
+						aGMListButtons = { 'button_feat_type' },
+						aPlayerListButtons = { 'button_feat_type' },
+						aCustomFilters = { ['Type'] = { sField = 'type' } },
+					}
+	)
 end
